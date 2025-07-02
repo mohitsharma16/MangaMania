10bb29b210a98559a28ed224a010bdbd14ba2053
mohitsharma16 <mohitsharmaofficial16@gmail.com>
2025-07-02 09:09:12 +0530

    feat: add UploadScreen.kt

--- app/src/main/java/com/example/mangamania/ui/screen/upload/UploadScreen.kt
+++ app/src/main/java/com/example/mangamania/ui/screen/upload/UploadScreen.kt
@@ -1,31 +1,31 @@
 package com.example.mangamania.ui.screen.upload
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import com.example.mangamania.ui.components.BaseScaffold
 import com.example.mangamania.ui.components.SectionTitle
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun UploadScreen(navController: NavHostController) {
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("Upload Manga") }) }
+    BaseScaffold(
+        navController = navController,
+        title = "Upload Manga"
     ) { contentPadding ->
         Column(
             modifier = Modifier
                 .padding(contentPadding)
                 .padding(16.dp)
         ) {
-            SectionTitle("Upload")
-            Text("Submit your own manga here!", color = MaterialTheme.colorScheme.onBackground)
+            SectionTitle(title = "Upload")
+            Text(
+                text = "Upload your own manga here!",
+                color = MaterialTheme.colorScheme.onBackground,
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
     }
 }
